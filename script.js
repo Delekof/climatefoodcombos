adc323ab3a3f0403fdd49c77dfa4418ebea7d90e
Delekof <pietro1.salvi@gmail.com>
2024-06-17 17:06:26 +0200

    Added presets

--- script.js
+++ script.js
@@ -145,6 +145,66 @@ fruitVegInput.addEventListener("keydown", function (event) {
   }
 });
 
+function setPreset(preset) {
+  const presets = {
+    highIncome: {
+      protein: 115,
+      calorie: 3500,
+      fruitVeg: 215,
+      emptyCalorie: 1350,
+    },
+    unitedStates: {
+      protein: 124,
+      calorie: 3900,
+      fruitVeg: 220,
+      emptyCalorie: 1715,
+    },
+    china: {
+      protein: 125,
+      calorie: 3400,
+      fruitVeg: 440,
+      emptyCalorie: 605,
+    },
+    india: {
+      protein: 70,
+      calorie: 2570,
+      fruitVeg: 160,
+      emptyCalorie: 590,
+    },
+    health: {
+      protein: 120,
+      calorie: 2500,
+      fruitVeg: 500,
+      emptyCalorie: 0,
+    },
+    guidelines: {
+      protein: 100,
+      calorie: 2500,
+      fruitVeg: 400,
+      emptyCalorie: 500,
+    }
+  };
+
+  const selectedPreset = presets[preset];
+
+  proteinRange.value = selectedPreset.protein;
+  proteinInput.value = selectedPreset.protein;
+
+  calorieRange.value = selectedPreset.calorie;
+  calorieInput.value = selectedPreset.calorie;
+
+  emptycalorieRange.value = selectedPreset.emptyCalorie;
+  emptycalorieInput.value = selectedPreset.emptyCalorie;
+
+  fruitVegRange.value = selectedPreset.fruitVeg;
+  fruitVegInput.value = selectedPreset.fruitVeg;
+
+  wasteRange.value = selectedPreset.waste;
+  wasteInput.value = selectedPreset.waste;
+
+  updateProteinPerCalorieSupply();
+}
+
 // Initial display on page load
 displayProteinFoods();
 
