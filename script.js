897836f42b52d08123832f1f75d8e2ebe4273c5f
Delekof <pietro1.salvi@gmail.com>
2024-06-18 08:58:26 +0200

    added second table

--- script.js
+++ script.js
@@ -33,12 +33,23 @@ function calculateProteinRatio(proteinFood, carbFood, kappa) {
   }
 }
 
+function calculateRatioEmissions(proteinFood, carbFood, kappa) {
+  let calories = (calorieRange.value - emptycalorieRange.value - fruitVegRange.value)
+  let protein_emissions = calories*kappa * nutritional_data[proteinFood].emissions_per_kcal
+  let carb_emissions = (1-kappa)*calories * nutritional_data[carbFood].emissions_per_kcal
+  return (protein_emissions + carb_emissions)*365/1000
+}
+
 function displayProteinFoods() {
   let kappa = (proteinRange.value / (calorieRange.value - emptycalorieRange.value - fruitVegRange.value));
   let tableBody = document.querySelector('#ratiosTable tbody');
   let carbHeaderRow = document.getElementById('carbHeaderRow');
 
+  let tableBody2 = document.querySelector('#emissionsTable tbody');
+  let carbHeaderRow2 = document.getElementById('carbHeaderRow2');
+
   tableBody.innerHTML = ''; // Clear previous rows
+  tableBody2.innerHTML = ''; // Clear previous rows
 
   // Clear and rebuild the header row
   carbHeaderRow.innerHTML = '<th>Protein/Carb</th>';
@@ -48,25 +59,46 @@ function displayProteinFoods() {
     carbHeaderRow.appendChild(th);
   });
 
+  // Clear and rebuild the header row
+  carbHeaderRow2.innerHTML = '<th>Protein/Carb</th>';
+  carbFoods.forEach(carbFood => {
+    let th = document.createElement('th');
+    th.textContent = carbFood;
+    carbHeaderRow2.appendChild(th);
+  });
+
   proteinFoods.forEach(proteinFood => {
     let row = tableBody.insertRow();
     let cell1 = row.insertCell(0);
     cell1.textContent = proteinFood;
 
+    let row2 = tableBody2.insertRow();
+    let cell2 = row2.insertCell(0);
+    cell2.textContent = proteinFood;
+
     carbFoods.forEach(carbFood => {
       let cell = row.insertCell();
+      let cell2 = row2.insertCell();
       let proteinRatio = calculateProteinRatio(proteinFood, carbFood, kappa);
 
       if (isNaN(proteinRatio) || proteinRatio > 1) {
         cell.textContent = "F";
         cell.style.backgroundColor = `rgba(123, 123, 123,1)`;
+        cell2.textContent = "F";
+        cell2.style.backgroundColor = `rgba(123, 123, 123,1)`;
       } else if (proteinRatio < 0) {
       	cell.textContent = "0%";
         cell.style.backgroundColor = `rgba(123, 123, 123,0)`;
+        let colorValue2 = Math.round(calculateRatioEmissions(proteinFood, carbFood, 0).toFixed(2));
+        cell2.style.backgroundColor = `rgba(123, 0, 0, ${colorValue2 / 40})`; // Greenish color
+        cell2.textContent = calculateRatioEmissions(proteinFood, carbFood, 0).toFixed(2);
       } else {
         cell.textContent = String((100 * proteinRatio).toFixed(0)) + "%";
         let colorValue = Math.round(proteinRatio * 100);
         cell.style.backgroundColor = `rgba(0, 123, 107, ${colorValue / 100})`; // Greenish color
+        let colorValue2 = Math.round(calculateRatioEmissions(proteinFood, carbFood, proteinRatio).toFixed(2));
+        cell2.style.backgroundColor = `rgba(123, 0, 0, ${colorValue2 / 40})`; // Greenish color
+        cell2.textContent = calculateRatioEmissions(proteinFood, carbFood, proteinRatio).toFixed(2);
       }
     });
   });
